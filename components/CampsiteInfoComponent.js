50f3909805ad77f5b0abd8246e674675b3526506
mjstrz <strzelecki.monica7@gmail.com>
2022-02-02 21:29:48 -0500

    PanResponder API; gestures - CampsiteInfoComponent, recognizeDrag - Week 3 - 2.2.2022 - 21:29

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Text, View, ScrollView, FlatList,
-    Modal, Button, StyleSheet } from 'react-native';
+    Modal, Button, StyleSheet, Alert, PanResponder } from 'react-native';
 import { Card, Icon, Input, Rating } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -25,38 +25,71 @@ function RenderCampsite(props) {
 
     const {campsite} = props;
 
+    const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
+
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderEnd: (e, gestureState) => {
+            console.log('pan responder end', gestureState);
+            if (recognizeDrag(gestureState)) {
+                Alert.alert(
+                    'Add Favorite',
+                    'Are you sure you wish to add ' + campsite.name + ' to favorites?',
+                    [
+                        {
+                            text: 'Cancel',
+                            style: 'cancel',
+                            onPress: () => console.log('Cancel Pressed')
+                        },
+                        {
+                            text: 'OK',
+                            onPress: () => props.favorite ?
+                                console.log('Already set as a favorite') : props.markFavorite()
+                        }
+                    ],
+                    { cancelable: false }
+                );
+            }
+            return true;
+        }
+    });
+
     if (campsite) {
         return (
-            <Animatable.View animation='fadeInDown' duration={2000} delay={1000}> 
-                <Card
-                    featuredTitle={campsite.name}
-                    image={{uri: baseUrl + campsite.image}}>
+            <Animatable.View 
+                animation='fadeInDown' 
+                duration={2000} 
+                delay={1000}
+                {...panResponder.panHandlers}> 
+                    <Card
+                        featuredTitle={campsite.name}
+                        image={{uri: baseUrl + campsite.image}}>
 
-                    <Text style={{margin: 10}}>
-                        {campsite.description}
-                    </Text>
-                    
-                    <View style={styles.cardRow}>
+                        <Text style={{margin: 10}}>
+                            {campsite.description}
+                        </Text>
+                        
+                        <View style={styles.cardRow}>
+                            <Icon
+                                name={props.favorite ? 'heart' : 'heart-o'}
+                                type='font-awesome'
+                                color='#f50'
+                                raised
+                                reverse
+                                onPress={() => props.favorite ?
+                                    console.log('Already set as a favorite') : props.markFavorite()}
+                            />
                         <Icon
-                            name={props.favorite ? 'heart' : 'heart-o'}
-                            type='font-awesome'
-                            color='#f50'
-                            raised
-                            reverse
-                            onPress={() => props.favorite ?
-                                console.log('Already set as a favorite') : props.markFavorite()}
-                        />
-                    <Icon
-                            name={'pencil'}
-                            type='font-awesome'
-                            color='#5637DD'
-                            style={styles.cardItem}
-                            raised
-                            reverse
-                            onPress={() => props.onShowModal()}
-                        />
-                    </View>
-                </Card>
+                                name={'pencil'}
+                                type='font-awesome'
+                                color='#5637DD'
+                                style={styles.cardItem}
+                                raised
+                                reverse
+                                onPress={() => props.onShowModal()}
+                            />
+                        </View>
+                    </Card>
         </Animatable.View>
         );
     }
